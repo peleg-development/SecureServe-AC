29875317f2b04a5bfc9e63a43535f5adb3fefce3
Peleg <106535297+peleg-development@users.noreply.github.com>
2025-03-16 00:14:40 +0200

    Update module.js

--- SecureServe/module.js
+++ SecureServe/module.js
@@ -1,5 +1,3 @@
-// im not sure i did correctlly so dont install this module just yet until im sure this is ok :)
-
 if (GetCurrentResourceName() === "SecureServe") return;
 
 const encryption_key = "c4a2ec5dc103a3f730460948f2e3c01df39ea4212bc2c82f";
@@ -26,25 +24,31 @@ const _onNet = onNet;
 const _emitNet = emitNet;
 const events_to_listen = {};
 
-globalThis.onNet = (event_name, handler) => {
-    const enc_event_name = encryptDecrypt(event_name);
-    events_to_listen[event_name] = enc_event_name;
-    _onNet(event_name, () => {
-        const src = global.source;
-        if (GetPlayerPing(src) > 0) {
-            exports["SecureServe"].module_punish(src, `Tried triggering a restricted event: ${event_name} in resource: ${GetCurrentResourceName()}.`);
-        }
-    });
-    _onNet(enc_event_name, function() {
-        const src = global.source;
-        if (GetPlayerPing(src) > 0 && decrypt(enc_event_name) !== "add_to_trigger_list" && decrypt(enc_event_name) !== "check_trigger_list") {
-            emit(encryptDecrypt("check_trigger_list"), src, decrypt(enc_event_name), GetCurrentResourceName());
-        }
-        handler.apply(null, arguments);
-    });
-};
-
-globalThis.emitNet = (event_name, target, ...args) => {
-    const enc_event_name = encryptDecrypt(event_name);
-    return _emitNet(enc_event_name, target, ...args);
-};
+if (IsDuplicityVersion()) {
+    globalThis.onNet = (event_name, handler) => {
+        const enc_event_name = encryptDecrypt(event_name);
+        events_to_listen[event_name] = enc_event_name;
+        _onNet(event_name, () => {
+            const src = global.source;
+            if (GetPlayerPing(src) > 0) {
+                exports["SecureServe"].module_punish(src, `Tried triggering a restricted event: ${event_name} in resource: ${GetCurrentResourceName()}.`);
+            }
+        });
+        _onNet(enc_event_name, function () {
+            const src = global.source;
+            if (
+                GetPlayerPing(src) > 0 &&
+                decrypt(enc_event_name) !== "add_to_trigger_list" &&
+                decrypt(enc_event_name) !== "check_trigger_list"
+            ) {
+                emit(encryptDecrypt("check_trigger_list"), src, decrypt(enc_event_name), GetCurrentResourceName());
+            }
+            handler.apply(null, arguments);
+        });
+    };
+} else {
+    globalThis.emitNet = (event_name, ...args) => {
+        const enc_event_name = encryptDecrypt(event_name);
+        return _emitNet(enc_event_name, ...args);
+    };
+}
